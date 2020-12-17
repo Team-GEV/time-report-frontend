756a34eaf1b553f6e00bbe97799c5038bd240a69
George Wang <georgewang2048@gmail.com>
2020-12-17 16:42:18 -0500

    Revise profile.

--- src/services/ProfileServices.js
+++ src/services/ProfileServices.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import authHeader from './AuthHeader';
 
 class ProfileServices{
     loadEmployee(userid){
         let url = "http://localhost:8081/timesheet/getEmployeeByUserid/";
-        return axios.get( url + userid);
+        return axios.get( url + userid, { headers: authHeader() });
     }
 
     updateEmployee(data){
         let url ="http://localhost:8081/timesheet/updateEmployee";
-        return axios.post( url,data);
+        return axios.post(url,data, { headers: authHeader() });
     }
 }
 
